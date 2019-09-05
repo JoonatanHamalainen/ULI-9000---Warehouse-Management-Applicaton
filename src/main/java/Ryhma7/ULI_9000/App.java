e217dc940efcc8fdbd12c6aab2bf9c78efe4bf35
TorstiL <37159598+TorstiL@users.noreply.github.com>
2019-09-05 10:05:36 +0300

    Initial commit

---
+++ src/main/java/Ryhma7/ULI_9000/App.java
@@ -0,0 +1,13 @@
+package Ryhma7.ULI_9000;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
