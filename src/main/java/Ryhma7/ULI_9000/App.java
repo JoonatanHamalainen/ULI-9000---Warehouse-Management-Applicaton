b56c5bf80a0e918a8930962afc40ad86be4636a5
Torsti Laine <torsti.laine@metropolia.fi>
2019-09-18 11:40:25 +0300

    Käyttöliittymän ja sen näkymien asettaminen sekä css tiedoston käyttöön otto

--- src/main/java/Ryhma7/ULI_9000/App.java
+++ src/main/java/Ryhma7/ULI_9000/App.java
@@ -7,7 +7,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.RowConstraints;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Modality;
@@ -40,6 +43,7 @@ public class App extends Application {
 		
 			this.rootLayout =(BorderPane) loader.load();
 			Scene scene = new Scene(this.rootLayout);
+			scene.getStylesheets().add("StoragePalette.css");
 			
 			this.primaryStage.setScene(scene);
 			
@@ -56,26 +60,43 @@ public class App extends Application {
 	
 	public void showStorageLayout() {
 		try {
+			int height = 300;
+			int width = 600;
 			FXMLLoader loader = new FXMLLoader();
 			loader.setLocation(App.class.getResource("view/StorageLayout.fxml"));
 			BorderPane page = (BorderPane) loader.load();
-			GridPane grid = (GridPane) page.getChildren().get(1);
-			for (int i = 0; i<10;i++) {
+			GridPane grid = new GridPane();
+			grid.getStyleClass().add("storage-grid");
+			for (int i = 0;i<15;i++) {
+				ColumnConstraints column = new ColumnConstraints(30);
+				grid.getColumnConstraints().add(column);
+			}
+			for (int i = 0; i<10; i++) {
+				RowConstraints row = new RowConstraints(30);
+				grid.getRowConstraints().add(row);
+			}
+			for (int i = 0; i<15;i++) {
 				for(int j = 0; j<10; j++) {
-					Rectangle rectangle = new Rectangle();
-					Color paint = Color.GREY;
+					/*Rectangle rectangle = new Rectangle();
+					Color paint = Color.BLACK;
 					rectangle.setFill(paint);
 					grid.add(rectangle, i, j);
+					System.out.println("Rectangle added");*/
+					Pane pane = new Pane();
+					pane.getStyleClass().add("storage-grid-cell");
+					grid.add(pane, i, j);
 				}
 			}
-			Stage storageStage = new Stage();
-			storageStage.setTitle("Storage Layout");
-			storageStage.initModality(Modality.WINDOW_MODAL);
-			storageStage.initOwner(primaryStage);
-			Scene scene = new Scene(page);
-			storageStage.setScene(scene);
+			//Stage storageStage = new Stage();
+			//storageStage.setTitle("Storage Layout");
+			//storageStage.initModality(Modality.WINDOW_MODAL);
+			//storageStage.initOwner(primaryStage);
+			//Scene scene = new Scene(page);
+			//storageStage.setScene(scene);
 			
-			storageStage.show();
+			//storageStage.show();
+			page.setCenter(grid);
+			this.rootLayout.setCenter(page);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
