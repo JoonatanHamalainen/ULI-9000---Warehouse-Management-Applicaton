13a9ef0eb0fdf724932d991e290cb86eeba7eb43
Torsti Laine <torsti.laine@metropolia.fi>
2019-09-23 15:02:12 +0300

    Storage controllerin liitäntä main appiin & createShelf() metodi

--- src/main/java/Ryhma7/ULI_9000/controller/StorageController.java
+++ src/main/java/Ryhma7/ULI_9000/controller/StorageController.java
@@ -1,5 +1,9 @@
 package Ryhma7.ULI_9000.controller;
 
+import java.awt.Point;
+import java.util.ArrayList;
+import Ryhma7.ULI_9000.App;
+
 import Ryhma7.ULI_9000.model.Shelf;
 import Ryhma7.ULI_9000.model.Storage;
 import javafx.fxml.FXML;
@@ -13,23 +17,38 @@ public class StorageController {
 	@FXML
 	private TextField storageLength;
 	
-	Storage storage;
-	Shelf shelf;
+	private ArrayList<Point> selectedCells = new ArrayList<Point>();
+	
+	private App mainApp;
+	
+	private Storage storage;
+	private Shelf shelf;
 	// Varaston koko
 	// poista hylly
 	// muokkaa hyllyä. (lisää/poista ruutuja)
 	
+	public StorageController() {	
+	}
+	
+	public void setMainApp(App mainApp) {
+		this.mainApp = mainApp;
+	}
+	
 	@FXML
 	public void createStorage() {
-		System.out.println("Storage created!");
-		System.out.println("Dimensions:" + this.storageWidth.getText() + "x" + this.storageLength.getText());
+		System.out.println("Storage created!");	
+		System.out.println("Dimensions: " + storageWidth.getText() + "x" + storageLength.getText());
 		//storage = new Storage(4,4);
 	}
 	
 	@FXML
-	public void createShelf() {
-		System.out.println("Shelf Created!");
-		//shelf = new Shelf();
+	public void handleCreateShelf() {
+		Shelf shelf = new Shelf(this.selectedCells, "123");
+		System.out.println(shelf.getCellCoordinates() + ", " + shelf.getID());
+	}
+	
+	public void createShelf(ArrayList<Point> cellCoordinates) {
+		
 	}
 	
 	/*public void changeStorageSize(int width, int length) {
@@ -46,5 +65,15 @@ public class StorageController {
 	
 	public void removeShelfCell() {
 		
+	}
+	
+	public void cellSelected(Point coordinateXY) {
+		if(this.selectedCells.contains(coordinateXY)) {
+			this.selectedCells.remove(coordinateXY);
+		}else{
+			this.selectedCells.add(coordinateXY);
+		}
+		System.out.println(this.selectedCells.size());
+		System.out.println(this.selectedCells);
 	}
 }
